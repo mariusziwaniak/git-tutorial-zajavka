952aca8195936ff48c10663d1bbf9405c69a8542
Mariusz Iwaniak <mariusz.iwaniak@gmail.com>
2023-04-23 19:54:58 +0200

    feature-commit-1 with rebase

--- Person.java
+++ Person.java
@@ -1,12 +1,18 @@
 import java.math.BigDecimal;
 
 public class Person {
+
+    public static final String HEIGHT = "100";
+
     private String name;
     private BigDecimal salary;
+    private String pesel;
 
-    public Integer age(){
+    public Integer age() {
         return 40;
     }
 
-    public static final String HEIGHT = "100";
+    public String whatIsYourHobby() {
+        return "pizza";
+    }
 }
