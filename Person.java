86d4a8163c1d1d273a8dfaaa255e47dcf43344e7
Marqus Testowy <marqus@testowy.pl>
2023-04-24 22:03:13 +0200

    Person.java 2

--- Person.java
+++ Person.java
@@ -2,26 +2,11 @@ import java.math.BigDecimal;
 
 public class Person {
 
-    public static final String HEIGHT = "100";
-
-    private String name;
-    private BigDecimal salary;
-    private String pesel;
-    private String surname;
-
-    public Integer age() {
-        return 40;
-    }
-
-    public String whatIsYourHobby() {
-        return "pizza";
-    }
-
-    public void hello(){
+    public static void main(String[] args) {
         System.out.println("Hello!");
-    }
-
-    public String getName() {
-        return name;
+        System.out.println("Hello2!");
+        System.out.println("Hello3!");
+        System.out.println("Hello4!");
+        System.out.println("Hello5!");
     }
 }
