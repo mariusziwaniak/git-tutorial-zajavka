86fdd9a06bb68b11a1bdea2cd8e76e27d2ebfbea
Marqus Testowy <marqus@testowy.pl>
2023-04-24 22:16:34 +0200

    Person.java 4

--- Person.java
+++ Person.java
@@ -11,6 +11,14 @@ public class Person {
         System.out.println("Hello5!");
         System.out.println("Hello5!");
         System.out.println("Hello5!");
-        System.out.println("Hello5!");
+        System.out.println("Hello5!");        System.out.println("Hello!");
+        System.out.println("Hello2!");
+        System.out.println("Hello3!");
+        System.out.println("Hello4!");
+        System.out.println("Hello7!");
+        System.out.println("Hello7!");
+        System.out.println("Hello7!");
+        System.out.println("Hello7!");
+        System.out.println("Hello7!");
     }
 }
