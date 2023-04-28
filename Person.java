1d79d926874c01fb0f578dc86accdbe36d3d5d7f
Marek Bazowy <marek@bazowy.xyz>
2023-04-28 19:40:12 +0200

    cherry-pick-1

--- Person.java
+++ Person.java
@@ -3,6 +3,14 @@ import java.math.BigDecimal;
 public class Person {
 
     public static void main(String[] args) {
+        //  cherry-pick
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
         System.out.println("Hello!");
     }
 }
