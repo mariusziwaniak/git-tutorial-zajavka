50e8f54e518bec88d10db4ad9a59a8a783ccd668
Marek Bazowy <marek@bazowy.xyz>
2023-04-30 21:14:03 +0200

    Main.java change MR to PR

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!");
         //  Pull Request || Merge Request
-        //  feature-branch -> origin/feature-branch -> MR -> Gradle build ->
+        //  feature-branch -> origin/feature-branch -> PR -> Gradle build ->
         //  approve -> (origin/feature-branch => origin/master) -> git checkout master -> git pull
     }
 }
