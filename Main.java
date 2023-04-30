c248a7280235c018134f428d1ba548ae96e76d6c
Marek Bazowy <marek@bazowy.xyz>
2023-04-30 21:23:28 +0200

    commit: hello Marek

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ public class Main {
         //  Pull Request || Merge Request
         //  feature-branch -> origin/feature-branch -> PR -> Gradle build ->
         //  approve -> (origin/feature-branch => origin/master) -> git checkout master -> git pull
+
+        System.out.println("Hello I'm Marek");
     }
 }
